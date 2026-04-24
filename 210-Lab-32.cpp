2b1cedaf6fdd24cdbd707b6aa80ba0af66493984
JoaquinF <joaquinpfelix@gmail.com>
2026-04-24 14:52:39 -0700

    Check 2

--- 210-Lab-32.cpp
+++ 210-Lab-32.cpp
@@ -1,1 +1,35 @@
 // COMSC-210 | Lab 32 | Joaquin Felix
+
+#include <iostream>
+#include <deque>
+#include "Car.h"
+using namespace std;
+
+int main ()
+{
+    const int INITIAL_SIZE = 2;
+
+    deque<Car> lane;
+
+    for (int i = 1; i <= INITIAL_SIZE; i++)
+    {
+        lane.push_back(Car());
+    }
+
+    cout << "Initial queue:\n";
+    for (auto car : lane)
+    {
+        car.print();
+    }
+
+    cout << "Removing one car (front)...\n";
+    lane.pop_front();
+
+    cout << "Queue after pop:\n";
+    for (auto car : lane)
+    {
+        car.print();
+    }
+
+    return 1;
+}
